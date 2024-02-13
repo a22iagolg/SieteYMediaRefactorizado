9793a35dcccf09b0caedc034667f7c82e5e6cf22
a22iagolg <a22iagolg@iessanclemente.net>
2024-02-13 20:01:30 +0100

    Finalizado

--- sieteymedia/InterfaceConsola.java
+++ sieteymedia/InterfaceConsola.java
@@ -1,30 +1,25 @@
 package sieteymedia;
 
 import java.util.Scanner;
+
 import recursos.Baraja;
 import recursos.Carta;
 
-public class GameControler {
-    Baraja baraja;
-    Carta[] cartasJugador;
-    Carta[] cartasBanca;
-    Scanner sc = new Scanner(System.in);
+public class InterfaceConsola {
 
-    public GameControler() {
-        baraja = new Baraja();
-        baraja.barajar();
-        // se van pidiendo cartas al jugar pero matemáticamente a partir de 15 siempre
-        // nos pasamos
-        // hay 12 cartas de medio puntos, si sacara estas 12 luego cartas con valor 1
-        // vemos que a partir de 15 cartas siempre se pasas
-        cartasJugador = new Carta[15];
-        cartasBanca = new Carta[15];
+    SieteYMedia juego = new SieteYMedia();
+    Scanner sc = new Scanner(System.in);
+    Carta[] cartasJugador = juego.cartasJugador;
+    Carta[] cartasBanca = juego.cartasBanca;
+    Baraja baraja = juego.baraja;
+
+    public InterfaceConsola() {
         presentarJuego();
         jugar();
     }
 
     public static void main(String[] args) {
-        new GameControler();
+        new InterfaceConsola();
     }
 
     void presentarJuego() {
@@ -58,14 +53,14 @@ public class GameControler {
         char opc = 'C';
         // obligamos a que como mínimo se tenga 1 carta
         System.out.println("Como mínimo recibes una carta, luego puedes decidir si seguir o plantarte");
-        while (valorCartas(cartasJugador) < 7.5 && opc == 'C') {
+        while (juego.valorCartas(cartasJugador) < 7.5 && opc == 'C') {
             Carta c = baraja.darCartas(1)[0];
             // insertamos c en las cartas del jugador
-            insertarCartaEnArray(cartasJugador, c);
+            juego.insertarCartaEnArray(cartasJugador, c);
             // mostramos cartas y su valor, si se pasa se sale del bucle
             System.out.println("Éstas son tus cartas jugador:");
             mostrarCartas(cartasJugador);
-            double valor = valorCartas(cartasJugador);
+            double valor = juego.valorCartas(cartasJugador);
             System.out.println("\n\tValor de cartas: " + valor);
             if (valor < 7.5) {
                 // suponemos que el usuario teclea bien !!!
@@ -79,7 +74,7 @@ public class GameControler {
 
     void turnoBanca() {
         // lo primero es consultar el valor que alcanzó el jugador en su turno
-        double valorCartasJugador = valorCartas(cartasJugador);
+        double valorCartasJugador = juego.valorCartas(cartasJugador);
         if (valorCartasJugador > 7.5) {
             System.out.println("Jugador, te has pasado en tu jugada anterior, gana la banca");
             return;
@@ -87,41 +82,18 @@ public class GameControler {
         System.out.println("\n\nTurno de banca ...");
 
         // juega hasta empatar o superar
-        while (valorCartas(cartasBanca) < valorCartasJugador) {
+        while (juego.valorCartas(cartasBanca) < valorCartasJugador) {
             Carta c = baraja.darCartas(1)[0];
-            insertarCartaEnArray(cartasBanca, c);
+            juego.insertarCartaEnArray(cartasBanca, c);
         }
         System.out.println("Éstas son mis cartas:");
         mostrarCartas(cartasBanca);
-        System.out.println("\nValor de  mis cartas(banca): " + valorCartas(cartasBanca));
-        if (valorCartas(cartasBanca) > 7.5) {
+        System.out.println("\nValor de  mis cartas(banca): " + juego.valorCartas(cartasBanca));
+        if (juego.valorCartas(cartasBanca) > 7.5) {
             System.out.println("me pasé, ganas tú,jugador");
         } else {
             System.out.println("Gana la banca");
         }
-    }
-
-    double valorCartas(Carta[] cartas) {
-        double total = 0.0;
-        int val;
-        int i = 0;
-        while (cartas[i] != null) {
-            val = cartas[i].getNumero();
-            total += (val > 7) ? 0.5 : val;
-            i++;
-        }
-
-        return total;
-    }
-
-    void insertarCartaEnArray(Carta[] cartas, Carta c) {
-        // inserta al final detectando el primer null
-        int i = 0;
-        while (cartas[i] != null) {
-            i++;
-        }
-        cartas[i] = c;
-
     }
 
     void mostrarCartas(Carta[] cartas) {
